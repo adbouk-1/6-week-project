37cc196970405c8cbbc2650d8068649422f716a5
adbouk-1 <alidbouk@live.com>
2024-06-10 12:42:43 +0100

    data preprocessing cleaned

--- src/functions/data_preprocessing.py
+++ src/functions/data_preprocessing.py
@@ -1,25 +1,45 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import os
-import seaborn as sns
 import datetime
 
 PRINT_INFO = False
 
-def check_is_boolean(df_column, yes_value, no_value):
-    new_col = df_column.replace({no_value: 0, yes_value: 1})
-    return new_col
 
 def convert_to_int(df_column):
+    """
+    Convert a column of a DataFrame to integer values using pandas.factorize.
+
+    Parameters:
+    df_column (pandas.Series): The column to be converted.
+
+    Returns:
+    new_col (numpy.ndarray): The converted column as an array of integers.
+    uniques (pandas.Index): The unique values in the original column.
+
+    """
+
+    #This was done before oneHotEncoding was used to convert the columns to integers. This was not used in the final analysis
     new_col, uniques = pd.factorize(df_column)
 
     return new_col, uniques
-def preprocess_csv(csv_file):
+def preProcessColumns(csv_file, convert_to_int=False):
+    """
+    Preprocesses the columns of a CSV file.
+
+    Args:
+        csv_file (str): The path to the CSV file.
+        convert_to_int (bool, optional): Indicates whether to convert string columns to integers. Defaults to False.
+
+    Returns:
+        tuple: A tuple containing two DataFrames:
+            - df: The preprocessed DataFrame.
+            - df_names: A DataFrame containing the unique values for each converted column.
+
+    """
+
     df = pd.read_csv(csv_file)
     df_names = pd.DataFrame()
 
-    #Add emergency readmission boolean
+    # Create new columns for the target variables
     df['IsReadmitted'] = df['EmergencyReadmissionDateTime'].apply(lambda x: 0 if pd.isnull(x) else 1)
 
     df['MonthsAwayFromDeath'] = df['BKProviderSpellNumber']
@@ -31,36 +51,35 @@ def preprocess_csv(csv_file):
 
     df['IsDeadWithin4Months'] = df['MonthsAwayFromDeath'].apply(lambda x: 1 if x<5 else 0)
 
-    #change each column to integer if it is a string
-    for column in df.columns:
-        if df[column].dtype == 'object':
-            df[column], unique_EthnicOriginCode = convert_to_int(df[column])
-            if len(unique_EthnicOriginCode) > len(df_names):
-                new_index = range(len(unique_EthnicOriginCode))  # New index based on the longer Series
-                df_names = df_names.reindex(new_index)
-            else:
-                unique_EthnicOriginCode = unique_EthnicOriginCode.to_series()
-            df_names[column] = unique_EthnicOriginCode
+    if convert_to_int:
+        # Change each column to integer if it is a string
+        for column in df.columns:
+            if df[column].dtype == 'object':
+                df[column], unique_values = convert_to_int(df[column])
+                # If the new column has more unique values than the original DataFrame, reindex the DataFrame to make sure it can be concatenated
+                if len(unique_values) > len(df_names):
+                    new_index = range(len(unique_values))  # New index based on the longer Series
+                    df_names = df_names.reindex(new_index)
+                else:
+                    unique_values = unique_values.to_series()
+                df_names[column] = unique_values
+    else:
+        df_names = None
 
-    for dirname, _, filenames in os.walk('src/data/Original csvs/'):
-        for filename in filenames:
-            filepath = os.path.join(dirname, filename)
-            print(filepath)
-            new_df, df_names = preprocess_csv(filepath)
+    return df, df_names
 
-            new_df.to_csv('src/data/unprocessed_csvs/' + filename.split('.')[0] + '_unprocessed.csv')
-
-def pickFileName(file_number):
-    for dirname, _, filenames in os.walk('src/data'):
-        for filename in filenames:
-            if PRINT_INFO:
-                print(os.path.join(dirname, filename))
-
-
-    filepath = os.path.join(dirname, filenames[file_number])
-    return filepath
 
 def readCSV(filepath):
+    """
+    Reads a CSV file and returns a pandas DataFrame.
+
+    Parameters:
+    filepath (str): The path to the CSV file.
+
+    Returns:
+    pandas.DataFrame: The DataFrame containing the data from the CSV file.
+    """
+
     df = pd.read_csv(filepath)
 
     if PRINT_INFO:
@@ -70,7 +89,6 @@ def readCSV(filepath):
         categorical = [var for var in df.columns if df[var].dtype=='O']
 
         print('There are {} categorical variables\n'.format(len(categorical)))
-
         print('The categorical variables are :\n\n', categorical)
         
         for var in categorical: 
@@ -81,31 +99,43 @@ def readCSV(filepath):
         print('There are {} numerical variables\n'.format(len(numerical)))
         print('The numerical variables are :', numerical)
 
-
-
     return df
 
 
 def preProcessCSV(df: pd.DataFrame, drop_duplicates=False, y_column='IsDeadWithin4Months'):
+    """
+    Preprocesses a CSV DataFrame by performing various data cleaning and transformation steps.
 
-    print(df['IsDeadWithin4Months'].dtype!='O')
+    Args:
+        df (pd.DataFrame): The input DataFrame to be preprocessed.
+        drop_duplicates (bool, optional): Whether to drop duplicate rows based on the 'PatientPseudoNo' column. Defaults to False.
+        y_column (str, optional): The target column to be used as the y variable. Can be either 'IsDeadWithin4Months' or 'IsReadmitted'. Defaults to 'IsDeadWithin4Months'.
+
+    Returns:
+        tuple: A tuple containing the preprocessed X DataFrame and the y Series.
+    """
+
+    #There may be some corrupt data where the value is '#NUM!' instead of 'NO' or 2 instead of 0. These need to be replaced.
     if df['IsDeadWithin4Months'].dtype!='O':
         df['IsDeadWithin4Months'] = df['IsDeadWithin4Months'].replace(2, 0)
     else:
         df['IsDeadWithin4Months'] = df['IsDeadWithin4Months'].replace('#NUM!', "NO")
 
+    #Drop any duplicate patient rows from the dataset
     if drop_duplicates:
         df = df.drop_duplicates(subset=['PatientPseudoNo'])
 
     if PRINT_INFO:
         print(df['IsDeadWithin4Months'].value_counts())
     
+    #This is a list of all the columns that were removed from the model for various reasons. These are explained in the report.
     x = df.drop(['IsDeceased', 'EmergencyReadmissionDateTime', 'DateOfDeath', 'UniqueEpisodeID', 'BKProviderSpellNumber',
              'EpisodeStartDateTime', 'EpisodeEndDateTime', 'Ward', 'EthnicOrigin', 'SourceSystem', 'TreatmentFunction.1', 
              'AdmissionDate', 'EpisodeDiagnosisCodeList', 'WardStartDateTime', 'WardEndDateTime', 'EpisodeProcedureCodeList', 'PatientPseudoNo', 
              'TreatmentFunctionCode.1', 'MonthsAwayFromDeath', 'DischargeMethod', 'DischargeDestination', 'TreatmentFunction', 'TreatmentFunctionCombined', 'Unnamed: 0',
              'SpecialtyKey', 'IPRDepartment', 'EthnicOriginGroup'], axis=1)
     
+    #This is the target variable that we are trying to predict. It is either 'IsDeadWithin4Months' or 'IsReadmitted'
     if y_column == 'IsDeadWithin4Months':
         x = x.drop(['IsDeadWithin4Months'], axis=1)
         y = df['IsDeadWithin4Months']
--- src/main.py
+++ src/main.py
@@ -2,32 +2,41 @@ from functions.data_preprocessing import *
 from functions.models import *
 from functions.data_visualisation import *
 
-    # filepath = pickFileName(1)
-    # df = readCSV('src/data/unprocessed_csvs/M47_unprocessed.csv')
-    # x, y = preProcessCSV(df, drop_duplicates=False, y_column='IsDeadWithin4Months')
-    # graph_clustering(x)
-    # optimiseHyperParameters(x, y, classifier="RandomForest")
-    # X_train, X_test, y_train, y_test = splitTrainingSet(x, y, test_size = 0.3, random_state = 0, classifier="RandomForest")
-    # model = trainModel(X_train, X_test, y_train, y_test, classifier="RandomForest")
-    # test(X_train, X_test, y_train, y_test)
-    # plotConfusionMatrix(model, X_test, y_test, 'M47_unprocessed.csv')
-    # calculateClassProbabilities(model, X_test)
-    # plotROCCurve(model, X_test, y_test)
-    # averagePrecisionScore(model, X_test, y_test)  
-    for dirname, _, filenames in os.walk('src/data/unprocessed_csvs/'):
-        for filename in filenames:
-            try:
-                filepath = os.path.join(dirname, filename)
-                print(filepath)
-                df = readCSV(filepath)
-                x, y = preProcessCSV(df, drop_duplicates=False, y_column='IsDeadWithin4Months')
-                # X_train, X_test, y_train, y_test = splitTrainingSet(x, y, test_size = 0.3, random_state = 0, classifier="RandomForest")
-                # model = trainModel(X_train, X_test, y_train, y_test, classifier="RandomForest")
-                # plotConfusionMatrix(model, X_test, y_test, filename)
-            except:
-                continue
-    # randomForest(model)
+PRINT_INFO = False
 
-    # df = readCSV('src/data/preprocessed_csvs/M47_preprocessed.csv')
-    # x, y = preProcessCSV(df, drop_duplicates=False, y_column='IsDeadWithin4Months')
-    # p_values = create_ttest(x, y)
+for dirname, _, filenames in os.walk('src/data/Original csvs/'):
+    for filename in filenames:
+        filepath = os.path.join(dirname, filename)
+        print(filepath)
+        new_df, df_names = preProcessColumns(filepath)
+
+        new_df.to_csv('src/data/unprocessed_csvs/' + filename.split('.')[0] + '_unprocessed.csv')
+
+# df = readCSV('src/data/unprocessed_csvs/M47_unprocessed.csv')
+# x, y = preProcessCSV(df, drop_duplicates=False, y_column='IsDeadWithin4Months')
+# graph_clustering(x)
+# optimiseHyperParameters(x, y, classifier="RandomForest")
+# X_train, X_test, y_train, y_test = splitTrainingSet(x, y, test_size = 0.3, random_state = 0, classifier="RandomForest")
+# model = trainModel(X_train, X_test, y_train, y_test, classifier="RandomForest")
+# test(X_train, X_test, y_train, y_test)
+# plotConfusionMatrix(model, X_test, y_test, 'M47_unprocessed.csv')
+# calculateClassProbabilities(model, X_test)
+# plotROCCurve(model, X_test, y_test)
+# averagePrecisionScore(model, X_test, y_test)  
+for dirname, _, filenames in os.walk('src/data/unprocessed_csvs/'):
+    for filename in filenames:
+        try:
+            filepath = os.path.join(dirname, filename)
+            print(filepath)
+            df = readCSV(filepath)
+            x, y = preProcessCSV(df, drop_duplicates=False, y_column='IsDeadWithin4Months')
+            # X_train, X_test, y_train, y_test = splitTrainingSet(x, y, test_size = 0.3, random_state = 0, classifier="RandomForest")
+            # model = trainModel(X_train, X_test, y_train, y_test, classifier="RandomForest")
+            # plotConfusionMatrix(model, X_test, y_test, filename)
+        except:
+            continue
+# randomForest(model)
+
+# df = readCSV('src/data/preprocessed_csvs/M47_preprocessed.csv')
+# x, y = preProcessCSV(df, drop_duplicates=False, y_column='IsDeadWithin4Months')
+# p_values = create_ttest(x, y)
