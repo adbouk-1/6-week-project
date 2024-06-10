782b688d7b1b9becac7c96967e49c988026265e1
adbouk-1 <alidbouk@live.com>
2024-06-10 13:48:05 +0100

    cleaned up models.py file

--- src/functions/models.py
+++ src/functions/models.py
@@ -1,37 +1,65 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV # type: ignore
+from sklearn.linear_model import LogisticRegression # type: ignore
+from sklearn.ensemble import RandomForestClassifier # type: ignore
+from sklearn.metrics import accuracy_score # type: ignore
+from sklearn.naive_bayes import GaussianNB # type: ignore
+from sklearn import svm # type: ignore
+import community.community_louvain as community_louvain # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+import category_encoders as ce # type: ignore
+import networkx as nx # type: ignore
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
-import category_encoders as ce
-from sklearn.naive_bayes import GaussianNB
-from sklearn import svm
-import networkx as nx
-import community.community_louvain as community_louvain
+import numpy as np
 
+PRINT_INFO = False
 
 def splitTrainingSet(x, y, test_size = 0.3, random_state = 0):
-        
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state)
-     
-        categorical = [col for col in X_train.columns if X_train[col].dtypes == 'O']
-        encoder = ce.OneHotEncoder(cols=categorical)
+    """
+    Split the dataset into training and testing sets.
+    
+    Parameters:
+    x (pandas.DataFrame): The input features.
+    y (pandas.Series): The target variable.
+    test_size (float, optional): The proportion of the dataset to include in the test split. Default is 0.3.
+    random_state (int, optional): The seed used by the random number generator. Default is 0.
+    
+    Returns:
+    X_train (pandas.DataFrame): The training set features.
+    X_test (pandas.DataFrame): The testing set features.
+    y_train (pandas.Series): The training set target variable.
+    y_test (pandas.Series): The testing set target variable.
+    """
+    
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state)
 
-        X_train = encoder.fit_transform(X_train)
-        X_test = encoder.transform(X_test)
+    #One hot encode the categorical variables
+    categorical = [col for col in X_train.columns if X_train[col].dtypes == 'O']
+    encoder = ce.OneHotEncoder(cols=categorical)
 
-        return X_train, X_test, y_train, y_test
+    X_train = encoder.fit_transform(X_train)
+    X_test = encoder.transform(X_test)
+
+    return X_train, X_test, y_train, y_test
 
 def trainModel(X_train, X_test, y_train, y_test, classifier="LogisticRegression"):
+    """
+    Trains a machine learning model using the specified classifier.
+
+    Parameters:
+    - X_train (array-like): The training data features.
+    - X_test (array-like): The testing data features.
+    - y_train (array-like): The training data labels.
+    - y_test (array-like): The testing data labels.
+    - classifier (str, optional): The classifier to use. Defaults to "LogisticRegression".
+
+    Returns:
+    - model: The trained machine learning model.
+
+    """
 
     if classifier == "LogisticRegression":
-        # LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-        #                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-        #                    multi_class='warn', n_jobs=None, penalty='l2',
-        #                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-        #                    warm_start=False)
+        #Hyperparameters to use for each target variable
         #Readmission rate: 4.281332398719396
         #Dead within 4 months: 545.5594781168514    
         model = LogisticRegression(solver='liblinear', C=545.5594781168514, random_state=0, penalty='l1')
@@ -63,6 +91,7 @@ def trainModel(X_train, X_test, y_train, y_test, classifier="LogisticRegression"
             print(np.unique(y_pred))
             null_accuracy = (y_test.value_counts().max()/(y_test.value_counts().max()+y_test.value_counts().min()))
             print('Null accuracy score: {0:0.4f}'. format(null_accuracy))
+
     elif classifier == "NaiveBayes":
 
         model = GaussianNB(var_smoothing=8.111308307896872e-05)
@@ -84,7 +113,9 @@ def trainModel(X_train, X_test, y_train, y_test, classifier="LogisticRegression"
             print(np.unique(y_pred))
             null_accuracy = (y_test.value_counts().max()/(y_test.value_counts().max()+y_test.value_counts().min()))
             print('Null accuracy score: {0:0.4f}'. format(null_accuracy))
+
     elif classifier=="SVM":
+        #NOTE: This doesnt work and was not attempted to be fixed
         model = svm.SVC(kernel='linear', C=1, random_state=0, verbose=1)
         model.fit(X_train, y_train)
 
@@ -93,12 +124,14 @@ def trainModel(X_train, X_test, y_train, y_test, classifier="LogisticRegression"
             print(model.intercept_)
             print(model.coef_)
 
-            model.predict_proba(x)
+            model.predict_proba(X_test)
 
-            model.predict(x)
+            model.predict(X_test)
 
             print(model.score(X_test, y_test))
+
     elif classifier=="RandomForest":
+        #Testing with limiting the max depth
         # model = RandomForestClassifier(max_depth=14, n_estimators=190)
         model = RandomForestClassifier()
         model.fit(X_train, y_train)
@@ -128,10 +161,22 @@ def trainModel(X_train, X_test, y_train, y_test, classifier="LogisticRegression"
 
 # Setup the logistic regression model
 def optimiseHyperParameters(x, y, classifier="LogisticRegression"):
+    """
+    Optimize hyperparameters for a given classifier using grid search or randomized search.
+
+    Parameters:
+    - x: The input features.
+    - y: The target variable.
+    - classifier: The classifier to use for hyperparameter optimization. Default is "LogisticRegression".
+
+    Returns:
+    None
+    """
 
     X_train, X_test, y_train, y_test = splitTrainingSet(x, y, test_size = 0.3, random_state = 0)
 
     if classifier == "LogisticRegression":
+
         model = LogisticRegression()
 
         # Define the grid of hyperparameters to search
@@ -143,7 +188,9 @@ def optimiseHyperParameters(x, y, classifier="LogisticRegression"):
 
         # Setup the grid search with cross-validation
         grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='accuracy', verbose=1, n_jobs=-1)
+
     elif classifier == "NaiveBayes":
+
         model = GaussianNB()
 
         # Define the grid of hyperparameters to search
@@ -153,7 +200,9 @@ def optimiseHyperParameters(x, y, classifier="LogisticRegression"):
 
         # Setup the grid search with cross-validation
         grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='accuracy', verbose=1, n_jobs=-1)
+
     elif classifier == "RandomForest":
+
         model = RandomForestClassifier()
 
         param_grid = {
@@ -161,12 +210,9 @@ def optimiseHyperParameters(x, y, classifier="LogisticRegression"):
             'max_depth': np.arange(65, 80, 1).tolist()
         }
 
-        grid_search = RandomizedSearchCV(model, 
-                                        param_distributions = param_grid, 
-                                        n_iter=5, 
-                                        cv=5)
+        grid_search = RandomizedSearchCV(model, param_distributions = param_grid, n_iter=5, cv=5)
 
-# Fit the random search object to the data
+    # Fit the random search object to the data
     # Perform grid search
     grid_search.fit(X_train, y_train)
 
@@ -182,10 +228,21 @@ def optimiseHyperParameters(x, y, classifier="LogisticRegression"):
     print("Test accuracy:", test_accuracy)
 
 
-def test(X_train, X_test, y_train, y_test):
-    random_state = 123
+def testRFDepth(X_train, X_test, y_train, y_test):
+    """
+    This function tests the Random Forest classifier with different depths.
 
-    output_tree = []
+    Parameters:
+    X_train (array-like): The training input samples.
+    X_test (array-like): The test input samples.
+    y_train (array-like): The target values for the training set.
+    y_test (array-like): The target values for the test set.
+
+    Returns:
+    None
+    """
+        
+    random_state = 123
     output_rf = []
 
     # build initial forest
@@ -193,11 +250,12 @@ def test(X_train, X_test, y_train, y_test):
         max_features="sqrt", random_state=random_state
     )
     initial_forest_fit = initial_forest.fit(X_train, y_train)
+
     # get max depth
     max_depth_rf = max(
         [estimator.get_depth() for estimator in initial_forest_fit.estimators_]
     )
-    # print(max_depth_rf)
+
     #construct depth grid
     list_depths_rf = np.arange(1, max_depth_rf, 1).tolist()
     depth_pct_rf = [100] + [(n / max_depth_rf) * 100 for n in list_depths_rf]
@@ -236,6 +294,17 @@ def test(X_train, X_test, y_train, y_test):
     df_rf.to_csv(path_or_buf="sim_shift_results_rf_check.csv", index=False)
 
 def graph_clustering(df):
+    """
+    Perform graph clustering on a DataFrame. Note that this function does not work, it still needs work.
+    It is a starting point for implementing graph clustering on the NHS data, but with the time constraints it was not possible to complete it.
+
+    Parameters:
+    - df (pandas.DataFrame): The input DataFrame.
+
+    Returns:
+    None
+    """
+
     # Create a graph
     G = nx.Graph()
 
